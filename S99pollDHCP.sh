b7fec68fb8d22c718735dd440e0b710ec4054a13
Gary Clayburg <gclaybur@comcast.net>
2016-09-17 11:49:46 -0600

    DSM 6.0 compatibility fix for ps command. should fix https://github.com/gclayburg/synology-diskstation-scripts/issues/14

--- S99pollDHCP.sh
+++ S99pollDHCP.sh
@@ -7,19 +7,29 @@
 # /var/services/homes/admin/diskstation_dns_modify.sh
 #
 # See Readme.md on github  https://github.com/gclayburg/synology-diskstation-scripts
+LOG_CONTEXT="-"  #override to add extra stuff to log messages
+date_echo(){
+    datestamp=$(date +%F_%T)
+    echo "${datestamp} ${LOG_CONTEXT} $*"
+}
 
+if $(ps x > /dev/null 2>&1 ); then
+  #apparently DSM 6.0 needs the x option for ps.  DSM 5.x does not have this option, but the default ps is good enough without options.
+  PS="ps x"
+else
+  PS="ps"
+fi
 
 if [ "$1" = "start"  ]; then
 #nohup ./poll-dhcp-changes.sh >> /var/services/homes/admin/logs/dhcp-dns.log 2>&1 &
 #nohup does not work on synology.
   ADMIN_DIR=/var/services/homes/admin
-  date
-  echo "is poll-dhcp-changes.sh running?"
-  POLL_RUNNING=`ps | grep poll-dhcp-changes | grep -v grep |wc -l`
+  date_echo "is poll-dhcp-changes.sh running?"
+  POLL_RUNNING=`$PS | grep poll-dhcp-changes | grep -v grep |wc -l`
   if [ $POLL_RUNNING -gt "0" ]; then
-    echo "poll-dhcp-changes already running."
+    date_echo "poll-dhcp-changes already running."
   else
-    echo "starting poll-dhcp-changes"
+    date_echo "starting poll-dhcp-changes"
     LOG_DIR=$ADMIN_DIR/logs
     if [ ! -e "$LOG_DIR" ]; then
       mkdir -p $LOG_DIR
@@ -28,9 +38,9 @@ if [ "$1" = "start"  ]; then
   fi
 
 elif [ "$1" = "stop" ]; then
-  MYPID=`ps | grep poll-dhcp-changes | grep -v grep | awk -F' ' '{print $1}'`
+  MYPID=`$PS | grep poll-dhcp-changes | grep -v grep | awk -F' ' '{print $1}'`
   if [ "$MYPID" -gt "1" ]; then
-    echo "killing PID: $MYPID"
+    date_echo "killing PID: $MYPID"
     kill $MYPID
   fi
 fi
--- startDHCP-DNS.sh
+++ startDHCP-DNS.sh
@@ -2,12 +2,24 @@
 #nohup ./poll-dhcp-changes.sh >> /volume1/homes/admin/logs/dhcp-dns.log 2>&1 &
 #nohup does not work on synology.  As a workaround, this script should be started from
 # DSM task scheduler.
-date
-echo "is poll-dhcp-changes.sh running?"
-POLL_RUNNING=`ps | grep poll-dhcp-changes | grep -v grep |wc -l`
-if [ $POLL_RUNNING -gt "0" ]; then
-  echo "poll-dhcp-changes already running."
+LOG_CONTEXT="-"  #override to add extra stuff to log messages
+date_echo(){
+    datestamp=$(date +%F_%T)
+    echo "${datestamp} ${LOG_CONTEXT} $*"
+}
+date_echo "is poll-dhcp-changes.sh running?"
+
+if $(ps x > /dev/null 2>&1 ); then
+  #apparently DSM 6.0 needs the x option for ps.  DSM 5.x does not have this option, but the default ps is good enough without options.
+  PS="ps x"
 else
-  echo "starting poll-dhcp-changes"
+  PS="ps"
+fi
+
+POLL_RUNNING=`$PS | grep poll-dhcp-changes | grep -v grep |wc -l`
+if [ $POLL_RUNNING -gt "0" ]; then
+  date_echo "poll-dhcp-changes already running."
+else
+  date_echo "starting poll-dhcp-changes"
   /volume1/homes/admin/poll-dhcp-changes.sh >> /volume1/homes/admin/logs/dhcp-dns.log 2>&1 
 fi
