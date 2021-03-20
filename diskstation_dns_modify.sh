20b467886de8323860547a6d2e29c914db8132bd
gracecoder-chris <cdjohnson.gracecode@gmail.com>
2021-03-20 09:53:33 -0500

    Remove ;1 patch as this also removes all SOA ints

--- diskstation_dns_modify.sh
+++ diskstation_dns_modify.sh
@@ -97,7 +97,6 @@ printPartialDNSFile () {
       {
         if (length($0) < 1) { next };
         if (match($0, ";dynamic") != 0) { next };
-        if (match($0, "[ \t]*1 ;") != 0) { next };
         print $0;
       }
    ' $1
