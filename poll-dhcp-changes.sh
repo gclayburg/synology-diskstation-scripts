35d56801de5ba275f1427b338da1823dce999142
Gary Clayburg <gclaybur@comcast.net>
2021-07-10 18:53:09 -0600

    fix for #34
    better error checking for DHCPD files
    Apparently DSM 7 may not always have a dhcpd-leases.log file

--- poll-dhcp-changes.sh
+++ poll-dhcp-changes.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 
-while true; do    
-   ATIME=`stat /etc/dhcpd/dhcpd-leases.log | grep Modify`
-   
-   if [[ "$ATIME" != "$LTIME" ]]; then
-     date
-     echo "dhcp leases changed - reloading DNS"
-     /var/services/homes/admin/diskstation_dns_modify.sh
-     LTIME=$ATIME
-   fi
-   sleep 5
+while true; do
+  reload_dns=false
+  if [ -r /etc/dhcpd/dhcpd-leases.log ]; then
+    LOGATIME=`stat /etc/dhcpd/dhcpd-leases.log | grep Modify`
+    if [ "$LOGATIME" != "$LOGLASTATIME" ]; then
+      date
+      LOGLASTATIME=$LOGATIME
+      reload_dns=true
+    fi
+  elif [ -r /etc/dhcpd/dhcpd.conf.leases ]; then
+    LEASEATIME=`stat /etc/dhcpd/dhcpd.conf.leases | grep Modify`
+    if [ "$LEASEATIME" != "$LEASELASTATIME" ]; then
+      date
+      LEASELASTATIME=$LEASEATIME
+      reload_dns=true
+    fi
+  else
+    echo "ERROR - No dhcp lease files found.  Is something misconfigured?"
+    exit 1
+  fi
+  if "$reload_dns"; then
+    echo "dhcp leases changed - reloading DNS"
+    /var/services/homes/admin/diskstation_dns_modify.sh
+  fi
+  sleep 5
 done
 
 
